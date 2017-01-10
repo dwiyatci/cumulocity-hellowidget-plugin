080ddcd8c741bf211e1ba1959a5a65c6f028705c
Glenn Dwiyatcita <dwiyatci@mail.com>
2017-01-10 11:25:20 +0100

    test(hello widget plugin): add missing unit tests for hello widget components

---
+++ plugins/hello-widget/hello-input.component.spec.js
@@ -0,0 +1,75 @@
+/**
+ * Created by glenn on 1/9/17.
+ */
+
+describe('helloWidgetApp.helloWidget: c8yHelloWidget component', function () {
+  var $injector;
+  var $rootScope;
+  var $compile;
+
+  beforeEach(function () {
+    common.globalBeforeWithUI();
+    module('helloWidgetApp.helloWidget');
+
+    inject(function (
+      _$injector_,
+      _$rootScope_,
+      _$compile_
+    ) {
+      $injector = _$injector_;
+      $rootScope = _$rootScope_;
+      $compile = _$compile_;
+    });
+  });
+
+  it('component should exist', function () {
+    expect($injector.has('c8yHelloTextInputDirective'))
+      .toEqual(true);
+  });
+
+  describe('configuring text', function () {
+    var $scope;
+    var element;
+
+    beforeEach(function () {
+
+      //////////// stubbing dependencies
+    });
+
+    it('should correctly configure text by default', function () {
+      // given
+      var helloText;
+
+      testConfiguringText(helloText, '');
+    });
+
+    it('should correctly configure text according to the text binding', function () {
+      // given
+      var helloText = 'halo, dunia';
+
+      testConfiguringText(helloText, helloText);
+    });
+
+    function testConfiguringText(helloText, expectedConfiguredText) {
+      // when
+      var template = '<c8y-hello-text-input hello-text="config.helloText"></c8y-hello-text-input>';
+      initComponent(template);
+
+      if (_.isString(helloText)) {
+        element.controller('c8yHelloTextInput').helloText = helloText;
+        $scope.$digest();
+      }
+
+      // then
+      expect($scope.config.helloText)
+        .toEqual(expectedConfiguredText);
+    }
+
+    function initComponent(template, bindings) {
+      $scope = _.assign($rootScope.$new(), bindings);
+
+      element = $compile(template)($scope);
+      $scope.$digest();
+    }
+  });
+});
---
+++ plugins/hello-widget/hello-widget.component.spec.js
@@ -0,0 +1,58 @@
+/**
+ * Created by glenn on 1/9/17.
+ */
+
+describe('helloWidgetApp.helloWidget: c8yHelloWidget component', function () {
+  var $injector;
+  var $componentController;
+
+  beforeEach(function () {
+    common.globalBeforeWithUI();
+    module('helloWidgetApp.helloWidget');
+
+    inject(function (
+      _$injector_,
+      _$componentController_
+    ) {
+      $injector = _$injector_;
+      $componentController = _$componentController_;
+    });
+  });
+
+  it('component should exist', function () {
+    expect($injector.has('c8yHelloWidgetDirective'))
+      .toEqual(true);
+  });
+
+  describe('displaying text', function () {
+    beforeEach(function () {
+
+      //////////// stubbing dependencies
+    });
+
+    it('should display "hello, world" by default', function () {
+      // given
+      var textBinding;
+
+      testDisplayingText(textBinding, 'hello, world');
+    });
+
+    it('should display text according to the text binding', function () {
+      // given
+      var textBinding = 'halo, dunia';
+
+      testDisplayingText(textBinding, textBinding);
+    });
+
+    function testDisplayingText(textBinding, expectedText) {
+      // when
+      var controller = $componentController(
+        'c8yHelloWidget', undefined, { text: textBinding });
+      controller.$onInit();
+
+      // then
+      expect(controller.text)
+        .toEqual(expectedText);
+    }
+  });
+});
