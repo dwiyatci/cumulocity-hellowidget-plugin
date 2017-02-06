b83504100a104efa251ca3c045112af10bafcc90
Glenn Dwiyatcita <dwiyatci@mail.com>
2017-02-06 14:32:34 +0100

    chore(test helper): "Got a black magic woman, I got a black magic woman" for test helper :p

--- test-helper.js
+++ test-helper.js
@@ -32,7 +32,29 @@
 
   function globalBefore() {
     module(function ($provide) {
-      $provide.value('info', {});
+      /*
+       * Best to keep the below "magic" fake info data for unit testing purpose,
+       * otherwise some services may not be able to be injected properly,
+       * errors maybe thrown from the minified (cumulocity-ui-build) files, and
+       * you don't wanna waste your time debugging the build files and will most
+       * likely have no clue on what's going wrong. You've been warned (!) :)
+       */
+      $provide.value('info', {
+        appKey: 'test',
+        token: 'test',
+        beta: 'test',
+        appConfig: { manifest: {} },
+        appName: 'test',
+        appLoadedJson: 'test',
+        logout: 'test',
+        hideAppEdit: 'test',
+        baseUrl: '/',
+        pageSize: 50,
+        preventGetUser: true,
+        languages: ['en'],
+        skipSwitchingToDefaultLanguage: true,
+        test: true
+      });
     });
     module('c8y.core');
     module('c8y.backendMocks');
